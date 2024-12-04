e18c0bcae75acad48058d39f14f056ac497028b8
Leonardomiranda03 <leorodrigomiranda13@gmail.com>
2024-12-04 17:59:23 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes';
+import bookRoutes from './routes/bookRoutes';
 
 dotenv.config();
 
@@ -11,9 +12,11 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/books', bookRoutes);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
